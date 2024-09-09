8701c5627345e1afd6585587e0e04c8cdcee028e
Schizy <ali.mechichi@gmail.com>
2024-09-09 12:49:39 +0200

    Chapitre 19

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -89,6 +89,11 @@ class DragonTreasure
     #[ApiFilter(BooleanFilter::class)]
     private bool $isPublished = false;
 
+    #[ORM\ManyToOne(inversedBy: 'dragonTreasures')]
+    #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['treasure:read', 'treasure:write'])]
+    private ?User $owner = null;
+
     public function __construct(string $name)
     {
         $this->name = $name;
@@ -185,6 +190,18 @@ class DragonTreasure
     public function setIsPublished(bool $isPublished): static
     {
         $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): static
+    {
+        $this->owner = $owner;
 
         return $this;
     }
